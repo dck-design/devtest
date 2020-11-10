f221524a2035d990991fe268d55d7671b140fc8e
dck-design <74109757+dck-design@users.noreply.github.com>
2020-11-10 12:56:46 +0700

    minor update

--- static/js/faviconAnimator.js
+++ static/js/faviconAnimator.js
@@ -1,14 +1,15 @@
+/* eslint-disable prettier/prettier */
 if (navigator.userAgent.indexOf("Firefox") < 0) {
-    var now_icon = 1, max_icon = 15, pause_timer = 0;
+    let nowIcon = 1; const maxIcon = 15; let pauseTimer = 0;
     setInterval(function () {
         if (document.querySelector("link[rel='icon']") !== null)
             document.querySelector("link[rel='icon']").remove();
         if (document.querySelector("link[rel='shortcut icon']") !== null)
             document.querySelector("link[rel='shortcut icon']").remove();
-        document.querySelector("head").insertAdjacentHTML("beforeend", '<link rel="icon" href="../favicon/favicon-' + now_icon + '.gif" type="image/gif">');
-        if (now_icon == 15) {
-            if (pause_timer > 30) { now_icon = 1; pause_timer = 0; }
-            else pause_timer++;
-        } else { now_icon++; }
+        document.querySelector("head").insertAdjacentHTML("beforeend", '<link rel="icon" href="../favicon/favicon-' + nowIcon + '.gif" type="image/gif">');
+        if (nowIcon === maxIcon) {
+            if (pauseTimer > 30) { nowIcon = 1; pauseTimer = 0; }
+            else pauseTimer++;
+        } else { nowIcon++; }
     }, 100);
 }
